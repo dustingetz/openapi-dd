40f4142e11e3dc337556e40ea7cb7d0b9517f106
Dustin Getz <dustin.getz@gmail.com>
2020-03-25 12:56:33 -0400

    web-client build configs

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,14 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from './components/Header';
+import Swagger from 'swagger-client';
+
 
 class HelloMessage extends React.Component {
+
+    state = {
+        foo: "bar"
+    };
+
+    testSwag = () => {
+        Swagger({ url: "http://petstore.swagger.io/v2/swagger.json" })
+            .then((client) => {
+                client
+                    .apis
+                    .pet // tag name == `pet`
+                    .addPet({ // operationId == `addPet`
+                        id: 1,
+                        body: {
+                            name: "bobby",
+                            status: "available"
+                        }
+                    })
+                    .then((a,b,c) => {
+                        debugger;
+
+                    })
+            })
+    };
+
     render() {
         return <div>
             <Header/>
             <div className="container">
                 <h1>Hello {this.props.name}</h1>
             </div>
+            <button onClick={this.testSwag}>swag!</button>
+            <pre>
+                {JSON.stringify(this.state, undefined, 2)}
+            </pre>
         </div>
     }
 }
