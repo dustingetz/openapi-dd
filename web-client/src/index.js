e9705bb0dc2c87a2f1e6a98cadca076ac14cc685
Dustin Getz <dustin.getz@gmail.com>
2020-03-25 14:00:42 -0400

    Swagger get example - fetch store inventory

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,36 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
-import Header from './components/Header';
 import Swagger from 'swagger-client';
 
 
-async function testSwagger (client, setState) {
-    const x = await client
-        .apis
-        .pet // tag name == `pet`
-        .addPet({ // operationId == `addPet`
-            id: 1,
-            body: {
-                name: "bobby",
-                status: "available"
-            }
-        });
-    setState({foo: x.body});
+async function testSwagger(client, setState) {
+    // tag name == `pet`; operationId == `addPet`
+    const x = await client.apis.pet.addPet({
+        id: 1,
+        body: {
+            name: "bobby",
+            status: "available"
+        }
+    });
+    setState({response: x.body});
+}
+
+async function fetchInventory (client, setState) {
+    const response = await client.apis.store.getInventory();
+    setState({response: response.body});
 }
 
 function HelloWorld ({client}) {
-    const [state, setState] = useState({foo: "bar", count: 0});
+    const [state, setState] = useState({response: {}, count: 0});
     return (
         <div>
             <h1>Test react working</h1>
             <p>You clicked {state.count} times</p>
-            <button onClick={() => setState({...state, count: state.count + 1})}>
+            <button onClick={() => setState({...state, count: (state.count || 0) + 1})}>
                 Click me
             </button>
-            <h1>Real stuff</h1>
-            <button onClick={testSwagger.bind(undefined, client, setState)}>swag!</button>
+            <h1>Swagger PetStore</h1>
+            <button onClick={testSwagger.bind(undefined, client, setState)}>Bobby!</button>
+            <button onClick={fetchInventory.bind(undefined, client, setState)}>Inventory!</button>
             <pre>
-                {JSON.stringify(state.foo, undefined, 2)}
+                {JSON.stringify(state, undefined, 2)}
             </pre>
         </div>
     );
