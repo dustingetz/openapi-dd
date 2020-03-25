37e4442bb5a915bd7378439d5c8842e9a3dc4cd1
Dustin Getz <dustin.getz@gmail.com>
2020-03-25 13:54:47 -0400

    add async/await and clean up swagger client init
    Fix useEffect infinite loop for client init

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,29 +1,24 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import Header from './components/Header';
 import Swagger from 'swagger-client';
 
 
-function testSwagger (setState) {
-    Swagger({ url: "http://petstore.swagger.io/v2/swagger.json" })
-        .then((client) => {
-            client
-                .apis
-                .pet // tag name == `pet`
-                .addPet({ // operationId == `addPet`
-                    id: 1,
-                    body: {
-                        name: "bobby",
-                        status: "available"
-                    }
-                })
-                .then((a,b,c) => {
-                    setState({foo: a.body})
-                })
-        })
+async function testSwagger (client, setState) {
+    const x = await client
+        .apis
+        .pet // tag name == `pet`
+        .addPet({ // operationId == `addPet`
+            id: 1,
+            body: {
+                name: "bobby",
+                status: "available"
+            }
+        });
+    setState({foo: x.body});
 }
 
-function HelloWorld () {
+function HelloWorld ({client}) {
     const [state, setState] = useState({foo: "bar", count: 0});
     return (
         <div>
@@ -33,7 +28,7 @@ function HelloWorld () {
                 Click me
             </button>
             <h1>Real stuff</h1>
-            <button onClick={testSwagger.bind(undefined, setState)}>swag!</button>
+            <button onClick={testSwagger.bind(undefined, client, setState)}>swag!</button>
             <pre>
                 {JSON.stringify(state.foo, undefined, 2)}
             </pre>
@@ -41,4 +36,19 @@ function HelloWorld () {
     );
 }
 
-ReactDOM.render(<HelloWorld/>, document.getElementById("app"));
+function Init () {
+    const [client, setClient] = useState(null);
+    useEffect(() => {
+        (async () => {
+            setClient(await Swagger({ url: "http://petstore.swagger.io/v2/swagger.json" }));
+        })();
+    }, []);
+
+    return (
+        client === null
+            ? <h1>Loading</h1>
+            : <HelloWorld client={client}/>
+    );
+}
+
+ReactDOM.render(<Init/>, document.getElementById("app"));
